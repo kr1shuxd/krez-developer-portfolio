22e56324e9621da57e5f5a9b0672bc094ada24c6
kr1shuxd <daddykrishu@gmail.com>
2025-03-27 23:56:14 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -27,7 +27,7 @@ export default function Footer() {
       <div className="container mx-auto flex flex-row items-center justify-between py-6">
         <span className="flex flex-row items-center space-x-4">
           <p className="text-xs text-muted-foreground">
-            Made with ❤️ by{""}
+            Made with ❤️ by{" "}
             <Link
               href="https://github.com/i.krezdevs"
               target="_blank"
