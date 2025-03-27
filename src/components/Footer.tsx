4447aeea6cd2566b1092f6d708ff405896ba7ec6
kr1shuxd <daddykrishu@gmail.com>
2025-03-27 23:58:05 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -27,14 +27,14 @@ export default function Footer() {
       <div className="container mx-auto flex flex-row items-center justify-between py-6">
         <span className="flex flex-row items-center space-x-4">
           <p className="text-xs text-muted-foreground">
-            Made with ❤️ by{" "}
+            Made with ❤️ by {" "}
             <Link
               href="https://github.com/i.krezdevs"
               target="_blank"
               passHref
               className="text-foreground transition hover:text-primary"
             >
-               Krez Devs
+              Krez Devs
             </Link>
           </p>
           <hr className="hidden h-6 border-l border-muted md:flex" />
