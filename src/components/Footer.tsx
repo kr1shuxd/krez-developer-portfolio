e6d60de7632c634097311e1614ca26d72bf43457
kr1shuxd <daddykrishu@gmail.com>
2025-03-27 23:57:10 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -34,7 +34,7 @@ export default function Footer() {
               passHref
               className="text-foreground transition hover:text-primary"
             >
-              Krez Devs
+               Krez Devs
             </Link>
           </p>
           <hr className="hidden h-6 border-l border-muted md:flex" />
