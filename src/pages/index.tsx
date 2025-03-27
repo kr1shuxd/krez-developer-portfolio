28e5fe5a71308d89b56fa51db47170b070868d6f
kr1shuxd <daddykrishu@gmail.com>
2025-03-27 23:08:56 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -187,7 +187,7 @@ export default function Home() {
                   <br />
                 </span>
                 <span className="clash-grotesk text-gradient text-6xl 2xl:text-8xl">
-                  WendoJ.
+                  Krez.
                 </span>
               </h1>
               <p
