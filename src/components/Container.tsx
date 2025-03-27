2a68e73545fba4ae768deafa3b13c4c4c71ae912
kr1shuxd <daddykrishu@gmail.com>
2025-03-27 23:54:39 +0530

    Update Container.tsx

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -82,8 +82,8 @@ export default function Container(props: ContainerProps) {
   const { children, ...customMeta } = props;
   const router = useRouter();
   const meta = {
-    title: "Wendo",
-    description: `Full-stack website developer and TypeScript enthusiast.`,
+    title: "Krez",
+    description: `Configuration developer and Management for servers.`,
     image: "/assets/logo.webp",
     type: "website",
     ...customMeta,
@@ -163,7 +163,7 @@ export default function Container(props: ContainerProps) {
           </button>
         </div>
         <Link href="/">
-          <span className="text-lg font-semibold">wendo</span>
+          <span className="text-lg font-semibold">Krez</span>
         </Link>
 
         {/* Desktop menu */}
@@ -220,7 +220,7 @@ export default function Container(props: ContainerProps) {
                 {/* Footer */}
                 <div className="flex min-h-fit w-full flex-col space-y-8 px-[22px] py-10">
                   <span className="text-sm text-muted-foreground">
-                    © {new Date().getFullYear()} wendo. All rights reserved.
+                    © {new Date().getFullYear()} krez. All rights reserved.
                   </span>
                 </div>
               </div>
